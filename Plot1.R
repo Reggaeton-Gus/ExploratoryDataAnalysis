8c7aabd8d8ccc59ba4da19bd00390957008598e5
Gustavo Escobar <escobarpalafox@gmail.com>
2017-01-27 14:35:37 +0000

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,19 @@
+## Initialise
+
+
+## Read data
+ data <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?")
+
+## Subset data for 2007-02-01 and 02-02-2007
+testdata <- data[which(data$Date == "1/2/2007" | data$Date == "2/2/2007"),]
+times <- testdata["Time"]
+times <- strptime(times[,"Time"],"%H:%M:%S" )
+dates <- testdata["Date"]
+dates <- as.Date (dates[,"Date"], "%d/%m/%y")
+testdata[["Date"]]<-dates
+testdata[["Time"]]<-times
+
+
+##PLOT 1
+GAP <- testdata$Global_active_power
+hist(GAP, xlab = "Global Active Power (kilowatts)", main = "Global Active Power", col = "red")
